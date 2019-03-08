5868618e15733b796182b862a7a42d879dc51a08
carolzhou <carol.zhou@comcast.net>
2019-03-08 08:11:14 -0800

    Fixing setup.py url, version

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@
 from setuptools import setup
 
 setup(name='multiphate',
-	version='0.1.2',
+	version='0.1.5',
 	description='Phage annotation pipeline',
 	author='Carol Zhou',
 	author_email='zhou4@llnl.gov',
-	url='https://github.com/carolzhou/multiPhATEb',
+	url='https://github.com/carolzhou/test_multiphate',
 	packages=[],
 	)
