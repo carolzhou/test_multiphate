a5363a25f41619ed7137c45b79c8eb4be665bc51
carolzhou <carol.zhou@comcast.net>
2019-03-09 17:59:01 -0800

    Adjusting entry points

--- setup.py
+++ setup.py
@@ -3,10 +3,15 @@
 from setuptools import setup
 
 setup(name='test_multiphate',
-	version='0.1.11',
+	version='0.1.12',
 	description='Phage annotation pipeline',
 	author='Carol Zhou',
 	author_email='zhou4@llnl.gov',
 	url='https://github.com/carolzhou/test_multiphate',
+        entry_points={
+            'console_scripts':[
+                'test_multiphate=test_multiphate.__main__:main',
+            ]
+        }
 	packages=[],
 	)
