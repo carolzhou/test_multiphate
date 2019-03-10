7adb0f07b04ce0b956d1086570dd5f31d7a54f31
carolzhou <carol.zhou@comcast.net>
2019-03-09 18:22:45 -0800

    Remove packages line

--- setup.py
+++ setup.py
@@ -8,7 +8,6 @@ setup(name='test_multiphate',
 	author='Carol Zhou',
 	author_email='zhou4@llnl.gov',
 	url='https://github.com/carolzhou/test_multiphate',
-	packages=[],
         entry_points={
             'console_scripts': [
                 'test_multiphate=test_multiphate.__main__:main',
