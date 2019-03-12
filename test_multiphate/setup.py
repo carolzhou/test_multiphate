c167f34364c4e00a281b9dbb6a08189bfcbcdbe6
carolzhou <carol.zhou@comcast.net>
2019-03-12 10:32:30 -0700

    Adding back setup.py to test_multiphate folder

---
+++ test_multiphate/setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='test_multiphate',
+	version='0.1.14',
+	description='Phage annotation pipeline',
+	author='Carol Zhou',
+	author_email='zhou4@llnl.gov',
+	url='https://github.com/carolzhou/test_multiphate',
+        entry_points={
+            'console_scripts': [
+                'test_multiphate=test_multiphate.__main__:main',
+            ],
+        },
+)
