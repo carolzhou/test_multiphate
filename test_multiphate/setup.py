2a45e236247f725d0975cc2809b06401e9ce7e34
carolzhou <carol.zhou@comcast.net>
2019-03-12 21:09:39 -0700

    Trying new console_scripts line

--- test_multiphate/setup.py
+++ test_multiphate/setup.py
@@ -8,9 +8,25 @@ setup(name='test_multiphate',
 	author='Carol Zhou',
 	author_email='zhou4@llnl.gov',
 	url='https://github.com/carolzhou/test_multiphate',
+        license='BSD-3',
+        keywords='phage genome annotation bioinformatics',
+        install_requires=[
+            'biopython',
+            'trnascan-se',
+            'glimmer',
+            'prodigal',
+            'hmmer',
+        ],
+        python_requires='~=3.4',
+        package_data={
+            'test_multiphate': ['README.md', "BSD-3.pdf'],
+        },
         entry_points={
             'console_scripts': [
-                'test_multiphate=test_multiphate.__main__:main',
+                'test_multiphate=__main__:main',
+            ],
+            'scripts': [
+                './test_multiphate',
             ],
         },
 )
