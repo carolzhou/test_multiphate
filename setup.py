10f6473bcdb24c48af1d701b7499b0e63cef78af
carolzhou <carol.zhou@comcast.net>
2019-03-12 21:38:37 -0700

    setup.py to main level

--- setup.py
+++ setup.py
@@ -3,14 +3,26 @@
 from setuptools import setup
 
 setup(name='test_multiphate',
-	version='0.1.14',
+	version='0.1.15',
 	description='Phage annotation pipeline',
 	author='Carol Zhou',
 	author_email='zhou4@llnl.gov',
 	url='https://github.com/carolzhou/test_multiphate',
+        license='BSD-3',
+        keywords='phage genome annotation bioinformatics',
+        install_requires=[
+            'biopython',
+            'trnascan-se',
+            'glimmer',
+            'prodigal',
+            'hmmer',
+        ],
+        python_requires='~=3.4',
+        package_data={
+            'test_multiphate': ['README.md', "BSD-3.pdf'],
+        },
         entry_points={
-            'console_scripts': [
-                'test_multiphate=test_multiphate.__main__:main',
-            ],
+            'console_scripts': ['test_multiphate=__main__:main']
+            'scripts': ['multiPhate'],
         },
 )
